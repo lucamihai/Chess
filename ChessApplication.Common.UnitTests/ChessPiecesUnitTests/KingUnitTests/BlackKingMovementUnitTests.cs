1a7b23f0e15704b20834d2cc443f75514e570e05
Mihai Luca <luca.mihai97@yahoo.com>
2019-05-03 16:35:42 +0300

    Added King movement unit tests

---
+++ ChessApplication.Common.UnitTests/ChessPiecesUnitTests/KingUnitTests/BlackKingMovementUnitTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessApplication.Common.ChessPieces;
+using ChessApplication.Common.Enums;
+using ChessApplication.Common.UserControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestsUtilities;
+
+namespace ChessApplication.Common.UnitTests.ChessPiecesUnitTests.KingUnitTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class BlackKingMovementUnitTests
+    {
+        private Box[,] ChessBoard;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            ChessBoard = ChessboardProvider.GetChessboardWithNoPieces();
+        }
+
+        [TestMethod]
+        public void BlackKingCanMove1BoxInAnyDirectionIfNotOccupied()
+        {
+            var blackKingPosition = new Point(3, 3);
+
+            ChessBoard[blackKingPosition.X, blackKingPosition.Y].Piece = new King(PieceColor.Black);
+            ChessBoard[blackKingPosition.X, blackKingPosition.Y].Piece
+                .CheckPossibilitiesForProvidedLocationAndMarkThem(ChessBoard, blackKingPosition, blackKingPosition);
+
+            var positionNorth = new Point(blackKingPosition.X + 1, blackKingPosition.Y);
+            var positionSouth = new Point(blackKingPosition.X - 1, blackKingPosition.Y);
+            var positionEast = new Point(blackKingPosition.X, blackKingPosition.Y + 1);
+            var positionWest = new Point(blackKingPosition.X, blackKingPosition.Y - 1);
+
+            var positionNorthEast = new Point(blackKingPosition.X + 1, blackKingPosition.Y + 1);
+            var positionSouthEast = new Point(blackKingPosition.X - 1, blackKingPosition.Y + 1);
+            var positionSouthWest = new Point(blackKingPosition.X - 1, blackKingPosition.Y - 1);
+            var positionNorthWest = new Point(blackKingPosition.X + 1, blackKingPosition.Y - 1);
+
+            Assert.IsTrue(ChessBoard[positionNorth.X, positionNorth.Y].Available);
+            Assert.IsTrue(ChessBoard[positionSouth.X, positionSouth.Y].Available);
+            Assert.IsTrue(ChessBoard[positionEast.X, positionEast.Y].Available);
+            Assert.IsTrue(ChessBoard[positionWest.X, positionWest.Y].Available);
+
+            Assert.IsTrue(ChessBoard[positionNorthEast.X, positionNorthEast.Y].Available);
+            Assert.IsTrue(ChessBoard[positionSouthEast.X, positionSouthEast.Y].Available);
+            Assert.IsTrue(ChessBoard[positionSouthWest.X, positionSouthWest.Y].Available);
+            Assert.IsTrue(ChessBoard[positionNorthWest.X, positionNorthWest.Y].Available);
+
+            Assert.AreEqual(8, Methods.GetNumberOfAvailableBoxes(ChessBoard));
+        }
+
+        [TestMethod]
+        public void BlackKingCantMoveOverOccupiedBoxesByBlackPiecesInAnyDirection()
+        {
+            var blackKingPosition = new Point(3, 3);
+
+            ChessBoard[blackKingPosition.X, blackKingPosition.Y].Piece = new King(PieceColor.Black);
+            Methods.SurroundBoxWithPawns(PieceColor.Black, blackKingPosition, ChessBoard);
+            ChessBoard[blackKingPosition.X, blackKingPosition.Y].Piece
+                .CheckPossibilitiesForProvidedLocationAndMarkThem(ChessBoard, blackKingPosition, blackKingPosition);
+
+            Assert.AreEqual(0, Methods.GetNumberOfAvailableBoxes(ChessBoard));
+        }
+    }
+}
---
+++ ChessApplication.Common.UnitTests/ChessPiecesUnitTests/KingUnitTests/WhiteKingMovementUnitTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using ChessApplication.Common.ChessPieces;
+using ChessApplication.Common.Enums;
+using ChessApplication.Common.UserControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestsUtilities;
+
+namespace ChessApplication.Common.UnitTests.ChessPiecesUnitTests.KingUnitTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class WhiteKingMovementUnitTests
+    {
+        private Box[,] ChessBoard;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            ChessBoard = ChessboardProvider.GetChessboardWithNoPieces();
+        }
+
+        [TestMethod]
+        public void WhiteKingCanMove1BoxInAnyDirectionIfNotOccupied()
+        {
+            var whiteKingPosition = new Point(3, 3);
+
+            ChessBoard[whiteKingPosition.X, whiteKingPosition.Y].Piece = new King(PieceColor.White);
+            ChessBoard[whiteKingPosition.X, whiteKingPosition.Y].Piece
+                .CheckPossibilitiesForProvidedLocationAndMarkThem(ChessBoard, whiteKingPosition, whiteKingPosition);
+
+            var positionNorth = new Point(whiteKingPosition.X + 1, whiteKingPosition.Y);
+            var positionSouth = new Point(whiteKingPosition.X - 1, whiteKingPosition.Y);
+            var positionEast = new Point(whiteKingPosition.X, whiteKingPosition.Y + 1);
+            var positionWest = new Point(whiteKingPosition.X, whiteKingPosition.Y - 1);
+
+            var positionNorthEast = new Point(whiteKingPosition.X + 1, whiteKingPosition.Y + 1);
+            var positionSouthEast = new Point(whiteKingPosition.X - 1, whiteKingPosition.Y + 1);
+            var positionSouthWest = new Point(whiteKingPosition.X - 1, whiteKingPosition.Y - 1);
+            var positionNorthWest = new Point(whiteKingPosition.X + 1, whiteKingPosition.Y - 1);
+
+            Assert.IsTrue(ChessBoard[positionNorth.X, positionNorth.Y].Available);
+            Assert.IsTrue(ChessBoard[positionSouth.X, positionSouth.Y].Available);
+            Assert.IsTrue(ChessBoard[positionEast.X, positionEast.Y].Available);
+            Assert.IsTrue(ChessBoard[positionWest.X, positionWest.Y].Available);
+
+            Assert.IsTrue(ChessBoard[positionNorthEast.X, positionNorthEast.Y].Available);
+            Assert.IsTrue(ChessBoard[positionSouthEast.X, positionSouthEast.Y].Available);
+            Assert.IsTrue(ChessBoard[positionSouthWest.X, positionSouthWest.Y].Available);
+            Assert.IsTrue(ChessBoard[positionNorthWest.X, positionNorthWest.Y].Available);
+
+            Assert.AreEqual(8, Methods.GetNumberOfAvailableBoxes(ChessBoard));
+        }
+
+        [TestMethod]
+        public void WhiteKingCantMoveOverOccupiedBoxesByWhitePiecesInAnyDirection()
+        {
+            var whiteKingPosition = new Point(3, 3);
+
+            ChessBoard[whiteKingPosition.X, whiteKingPosition.Y].Piece = new King(PieceColor.White);
+            Methods.SurroundBoxWithPawns(PieceColor.White, whiteKingPosition, ChessBoard);
+            ChessBoard[whiteKingPosition.X, whiteKingPosition.Y].Piece
+                .CheckPossibilitiesForProvidedLocationAndMarkThem(ChessBoard, whiteKingPosition, whiteKingPosition);
+
+            Assert.AreEqual(0, Methods.GetNumberOfAvailableBoxes(ChessBoard));
+        }
+    }
+}
