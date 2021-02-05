34dc4f5eb3f64d49cdb8e80fc0bf576a5277a336
Mihai Luca <luca.mihai97@yahoo.com>
2021-02-05 19:17:19 +0200

    Reduce repetitive code from AccessibleBoxesUtil

--- ChessApplication.Common/ChessPieces/Helpers/AccessibleBoxesUtil.cs
+++ ChessApplication.Common/ChessPieces/Helpers/AccessibleBoxesUtil.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using ChessApplication.Common.Interfaces;
-using ChessApplication.Common.UserControls;
 
 namespace ChessApplication.Common.ChessPieces.Helpers
 {
@@ -11,35 +9,20 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[location.X, location.Y];
 
-            for (int secondaryColumn = column; secondaryColumn >= 1; secondaryColumn--)
+            for (var secondaryColumn = column; secondaryColumn >= 1; secondaryColumn--)
             {
                 if (secondaryColumn == column)
+                {
                     continue;
-
-                var locationToBeInspected = chessBoard[row, secondaryColumn];
-                var destination = new Point(row, secondaryColumn);
-
-                if (locationToBeInspected.Piece == null)
-                {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
                 }
-                else
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
 
+                var boxToInspect = chessBoard[row, secondaryColumn];
+
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
+
+                if (boxToInspect.Piece != null)
+                {
                     break;
                 }
             }
@@ -49,35 +32,20 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[location.X, location.Y];
 
-            for (int secondaryColumn = column; secondaryColumn <= 8; secondaryColumn++)
+            for (var secondaryColumn = column; secondaryColumn <= 8; secondaryColumn++)
             {
                 if (secondaryColumn == column)
+                {
                     continue;
-
-                var locationToBeInspected = chessBoard[row, secondaryColumn];
-                var destination = new Point(row, secondaryColumn);
-
-                if (locationToBeInspected.Piece == null)
-                {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
                 }
-                else
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
 
+                var boxToInspect = chessBoard[row, secondaryColumn];
+
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
+
+                if (boxToInspect.Piece != null)
+                {
                     break;
                 }
             }
@@ -87,35 +55,20 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[location.X, location.Y];
 
-            for (int secondaryRow = row; secondaryRow >= 1; secondaryRow--)
+            for (var secondaryRow = row; secondaryRow >= 1; secondaryRow--)
             {
                 if (secondaryRow == row)
+                {
                     continue;
-
-                var locationToBeInspected = chessBoard[secondaryRow, column];
-                var destination = new Point(secondaryRow, column);
-
-                if (locationToBeInspected.Piece == null)
-                {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
                 }
-                else
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
 
+                var boxToInspect = chessBoard[secondaryRow, column];
+
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
+
+                if (boxToInspect.Piece != null)
+                {
                     break;
                 }
             }
@@ -125,35 +78,18 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[location.X, location.Y];
 
-            for (int secondaryRow = row; secondaryRow <= 8; secondaryRow++)
+            for (var secondaryRow = row; secondaryRow <= 8; secondaryRow++)
             {
                 if (secondaryRow == row)
                     continue;
 
-                var locationToBeInspected = chessBoard[secondaryRow, column];
-                var destination = new Point(secondaryRow, column);
+                var boxToInspect = chessBoard[secondaryRow, column];
 
-                if (locationToBeInspected.Piece == null)
-                {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
-                }
-                else
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
 
+                if (boxToInspect.Piece != null)
+                {
                     break;
                 }
             }
@@ -163,36 +99,20 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[row, column];
 
             for (int secondaryRow = row, secondaryColumn = column; secondaryRow >= 1 && secondaryColumn >= 1; secondaryRow--, secondaryColumn--)
             {
                 if (secondaryRow == row && secondaryColumn == column)
-                    continue;
-
-                var locationToBeInspected = chessBoard[secondaryRow, secondaryColumn];
-                var destination = new Point(secondaryRow, secondaryColumn);
-
-                if (locationToBeInspected.Piece == null)
                 {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
+                    continue;
                 }
 
-                if (locationToBeInspected.Piece != null)
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
+                var boxToInspect = chessBoard[secondaryRow, secondaryColumn];
 
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
+
+                if (boxToInspect.Piece != null)
+                {
                     break;
                 }
             }
@@ -202,36 +122,20 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[row, column];
 
             for (int secondaryRow = row, secondaryColumn = column; secondaryRow <= 8 && secondaryColumn <= 8; secondaryRow++, secondaryColumn++)
             {
                 if (secondaryRow == row && secondaryColumn == column)
-                    continue;
-
-                var locationToBeInspected = chessBoard[secondaryRow, secondaryColumn];
-                var destination = new Point(secondaryRow, secondaryColumn);
-
-                if (locationToBeInspected.Piece == null)
                 {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
+                    continue;
                 }
 
-                if (locationToBeInspected.Piece != null)
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
+                var boxToInspect = chessBoard[secondaryRow, secondaryColumn];
 
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
+
+                if (boxToInspect.Piece != null)
+                {
                     break;
                 }
             }
@@ -241,36 +145,20 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[row, column];
 
             for (int secondaryRow = row, secondaryColumn = column; secondaryRow <= 8 && secondaryColumn >= 1; secondaryRow++, secondaryColumn--)
             {
                 if (secondaryRow == row && secondaryColumn == column)
-                    continue;
-
-                var locationToBeInspected = chessBoard[secondaryRow, secondaryColumn];
-                var destination = new Point(secondaryRow, secondaryColumn);
-
-                if (locationToBeInspected.Piece == null)
                 {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
+                    continue;
                 }
 
-                if (locationToBeInspected.Piece != null)
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
+                var boxToInspect = chessBoard[secondaryRow, secondaryColumn];
 
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
+
+                if (boxToInspect.Piece != null)
+                {
                     break;
                 }
             }
@@ -280,37 +168,44 @@ namespace ChessApplication.Common.ChessPieces.Helpers
         {
             var row = location.X;
             var column = location.Y;
-            var startLocation = chessBoard[row, column];
 
             for (int secondaryRow = row, secondaryColumn = column; secondaryRow >= 1 && secondaryColumn <= 8; secondaryRow--, secondaryColumn++)
             {
                 if (secondaryRow == row && secondaryColumn == column)
-                    continue;
-
-                var locationToBeInspected = chessBoard[secondaryRow, secondaryColumn];
-                var destination = new Point(secondaryRow, secondaryColumn);
-
-                if (locationToBeInspected.Piece == null)
                 {
-                    if (!chessBoard.MoveTriggersCheck(location, destination))
-                    {
-                        locationToBeInspected.Available = true;
-                        startLocation.Piece.CanMove = true;
-                    }
+                    continue;
                 }
 
-                if (locationToBeInspected.Piece != null)
-                {
-                    if (locationToBeInspected.Piece.Color != startLocation.Piece.Color)
-                    {
-                        if (!chessBoard.MoveTriggersCheck(location, destination))
-                        {
-                            locationToBeInspected.Available = true;
-                            startLocation.Piece.CanMove = true;
-                        }
-                    }
+                var boxToInspect = chessBoard[secondaryRow, secondaryColumn];
 
+                MarkStartingBoxIfAccessible(chessBoard, location, boxToInspect.Position);
+
+                if (boxToInspect.Piece != null)
+                {
                     break;
+                }
+            }
+        }
+
+        private static void MarkStartingBoxIfAccessible(IChessboard chessBoard, Point startPosition, Point destinationPosition)
+        {
+            if (chessBoard[destinationPosition].Piece == null)
+            {
+                if (!chessBoard.MoveTriggersCheck(startPosition, destinationPosition))
+                {
+                    chessBoard[destinationPosition].Available = true;
+                    chessBoard[startPosition].Piece.CanMove = true;
+                }
+            }
+            else
+            {
+                if (chessBoard[destinationPosition].Piece.Color != chessBoard[startPosition].Piece.Color)
+                {
+                    if (!chessBoard.MoveTriggersCheck(startPosition, destinationPosition))
+                    {
+                        chessBoard[destinationPosition].Available = true;
+                        chessBoard[startPosition].Piece.CanMove = true;
+                    }
                 }
             }
         }
